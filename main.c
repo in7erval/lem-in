13b9165a484a01267a1dd0fca2cc16dfd4e935da
in7erval <yudakov.d@mail.ru>
2020-07-13 18:20:03 +0300

    fixed error

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ t_room	*new_room(char *name, int x, int y)
 	return room;
 }
 
-void	push_back_room(t_room **head, t_room *room) // Добавляет в конец списка новую комнату
+void	push_back_room(t_room **head, t_room *room)
 {
 	t_room *buf;
 
@@ -189,7 +189,7 @@ int 	is_in_union(t_room *room, t_room *check_room)
 	return (0);
 }
 
-void	add_union(t_room *rooms, char *room1_name, char *room2_name) // Добавляет связь между комнатами
+void	add_union(t_room *rooms, char *room1_name, char *room2_name)
 {
 	t_room *room1;
 	t_room *room2;
@@ -507,6 +507,22 @@ void	delete_input_forks(t_room *room)
 		}
 		buf = buf->next;
 	}
+	if (room->count_input > 1) //если все ребра "хорошие", то оставляем только одно
+	{
+		buf = input_links;
+		while (buf)
+		{
+			if (room->count_input > 1)
+			{
+				from_room = buf->room;
+				ft_delete_elem(&(from_room->aligned_union_room), room, input_fork_delete);
+				ft_delete_elem(&(from_room->union_room), room, input_fork_delete);
+				ft_delete_elem(&(room->union_room), from_room, input_fork_delete);
+				room->count_input--;
+			}
+			buf = buf->next;
+		}
+	}
 	free_rooms_list(&input_links);
 }
 
@@ -755,218 +771,18 @@ void	perform_pathes(t_list *pathes, int ants)
 
 }
 
+void perform_test(int num, t_room **rooms);
+
 int main()
 {
 	t_room *rooms;
 	t_list	*pathes;
-	int 	test_number = 8;
 
 	rooms = NULL;
-	if (test_number == 1)
-	{
-		push_back_room(&rooms, new_room("room_0", 10, 0));
-		push_back_room(&rooms, new_room("room_1", 10, 0));
-		push_back_room(&rooms, new_room("room_2", 10, 0));
-		push_back_room(&rooms, new_room("room_3", 10, 0));
-		push_back_room(&rooms, new_room("room_4", 10, 0));
-		push_back_room(&rooms, new_room("room_5", 10, 0));
-		find_room_by_name(rooms, "room_0")->status = START;
-		find_room_by_name(rooms, "room_5")->status = END;
-		add_union(rooms, "room_0", "room_1");
-		add_union(rooms, "room_0", "room_2");
-		add_union(rooms, "room_0", "room_3");
-		add_union(rooms, "room_2", "room_1");
-		add_union(rooms, "room_2", "room_3");
-		add_union(rooms, "room_4", "room_1");
-		add_union(rooms, "room_3", "room_4");
-		add_union(rooms, "room_2", "room_4");
-		add_union(rooms, "room_4", "room_5");
-	}
-	if (test_number == 2)
-	{
-		push_back_room(&rooms, new_room("room_0", 10, 0));
-		push_back_room(&rooms, new_room("room_1", 10, 5));
-		push_back_room(&rooms, new_room("room_2", 15, 5));
-		push_back_room(&rooms, new_room("room_3", 5, 10));
-		push_back_room(&rooms, new_room("room_4", 10, 10));
-		push_back_room(&rooms, new_room("room_5", 20, 10));
-		push_back_room(&rooms, new_room("room_6", 0, 15));
-		push_back_room(&rooms, new_room("room_7", 5, 17));
-		push_back_room(&rooms, new_room("room_8", 0, 20));
-		push_back_room(&rooms, new_room("room_9", 10, 25));
-		push_back_room(&rooms, new_room("room_10", 15, 25));
-		find_room_by_name(rooms, "room_0")->status = START;
-		find_room_by_name(rooms, "room_9")->status = END;
-		add_union(rooms, "room_0", "room_1");
-		add_union(rooms, "room_0", "room_2");
-		add_union(rooms, "room_2", "room_5");
-		add_union(rooms, "room_2", "room_4");
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_1", "room_4");
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_4", "room_3");
-		add_union(rooms, "room_3", "room_6");
-		add_union(rooms, "room_3", "room_7");
-		add_union(rooms, "room_4", "room_9");
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_6", "room_8");
-		add_union(rooms, "room_8", "room_9");
-		add_union(rooms, "room_7", "room_9");
-		add_union(rooms, "room_9", "room_10");
-	}
-	if (test_number == 3)
-	{
-		push_back_room(&rooms, new_room("room_1", 10, 5));
-		push_back_room(&rooms, new_room("room_2", 15, 5));
-		push_back_room(&rooms, new_room("room_3", 5, 10));
-		push_back_room(&rooms, new_room("room_4", 10, 10));
-		push_back_room(&rooms, new_room("room_5", 20, 10));
-		push_back_room(&rooms, new_room("room_6", 0, 15));
-		push_back_room(&rooms, new_room("room_7", 5, 17));
-		find_room_by_name(rooms, "room_1")->status = START;
-		find_room_by_name(rooms, "room_7")->status = END;
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_1", "room_6");
-		add_union(rooms, "room_1", "room_4");
-		add_union(rooms, "room_2", "room_3");
-		add_union(rooms, "room_2", "room_4");
-		add_union(rooms, "room_4", "room_6");
-		add_union(rooms, "room_2", "room_5");
-		add_union(rooms, "room_6", "room_5");
-		add_union(rooms, "room_6", "room_7");
-	}
-	if (test_number == 4)
-	{
-		push_back_room(&rooms, new_room("room_1", 10, 5));
-		push_back_room(&rooms, new_room("room_2", 15, 5));
-		push_back_room(&rooms, new_room("room_3", 5, 10));
-		push_back_room(&rooms, new_room("room_4", 10, 10));
-		push_back_room(&rooms, new_room("room_5", 20, 10));
-		push_back_room(&rooms, new_room("room_6", 0, 15));
-		find_room_by_name(rooms, "room_1")->status = START;
-		find_room_by_name(rooms, "room_6")->status = END;
-		add_union(rooms, "room_1", "room_2");
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_2", "room_3");
-		add_union(rooms, "room_2", "room_4");
-		add_union(rooms, "room_4", "room_5");
-		add_union(rooms, "room_5", "room_6");
-	}
-	if (test_number == 5)
-	{
-		push_back_room(&rooms, new_room("room_1", 10, 5));
-		push_back_room(&rooms, new_room("room_2", 15, 5));
-		push_back_room(&rooms, new_room("room_3", 5, 10));
-		push_back_room(&rooms, new_room("room_4", 10, 10));
-		push_back_room(&rooms, new_room("room_5", 20, 10));
-		push_back_room(&rooms, new_room("room_6", 0, 15));
-		push_back_room(&rooms, new_room("room_7", 5, 17));
-		push_back_room(&rooms, new_room("room_8", 0, 20));
-		push_back_room(&rooms, new_room("room_9", 10, 25));
-		push_back_room(&rooms, new_room("room_10", 15, 25));
-		push_back_room(&rooms, new_room("room_11", 15, 25));
-		push_back_room(&rooms, new_room("room_12", 15, 25));
-		push_back_room(&rooms, new_room("room_13", 15, 25));
-		find_room_by_name(rooms, "room_1")->status = START;
-		find_room_by_name(rooms, "room_13")->status = END;
-		add_union(rooms, "room_1", "room_2");
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_2", "room_3");
-		add_union(rooms, "room_2", "room_4");
-		add_union(rooms, "room_3", "room_6");
-		add_union(rooms, "room_4", "room_6");
-		add_union(rooms, "room_1", "room_5");
-		add_union(rooms, "room_4", "room_7");
-		add_union(rooms, "room_6", "room_8");
-		add_union(rooms, "room_7", "room_5");
-		add_union(rooms, "room_5", "room_8");
-		add_union(rooms, "room_5", "room_9");
-		add_union(rooms, "room_9", "room_10");
-		add_union(rooms, "room_9", "room_11");
-		add_union(rooms, "room_10", "room_12");
-		add_union(rooms, "room_11", "room_12");
-		add_union(rooms, "room_12", "room_13");
-	}
-	if (test_number == 6)
-	{
-		push_back_room(&rooms, new_room("room_1", 10, 5));
-		push_back_room(&rooms, new_room("room_2", 15, 5));
-		push_back_room(&rooms, new_room("room_3", 5, 10));
-		push_back_room(&rooms, new_room("room_4", 10, 10));
-		push_back_room(&rooms, new_room("room_5", 20, 10));
-		push_back_room(&rooms, new_room("room_6", 0, 15));
-		push_back_room(&rooms, new_room("room_7", 0, 15));
-		push_back_room(&rooms, new_room("room_8", 0, 15));
-		push_back_room(&rooms, new_room("room_9", 0, 15));
-		find_room_by_name(rooms, "room_1")->status = START;
-		find_room_by_name(rooms, "room_6")->status = END;
-		add_union(rooms, "room_1", "room_2");
-		add_union(rooms, "room_1", "room_4");
-		add_union(rooms, "room_2", "room_3");
-		add_union(rooms, "room_3", "room_6");
-		add_union(rooms, "room_4", "room_5");
-		add_union(rooms, "room_5", "room_7");
-		add_union(rooms, "room_7", "room_8");
-		add_union(rooms, "room_8", "room_9");
-		add_union(rooms, "room_9", "room_6");
-	}
-	if (test_number == 7)
-	{
-		push_back_room(&rooms, new_room("room_0", 10, 5));
-		push_back_room(&rooms, new_room("room_1", 10, 5));
-		push_back_room(&rooms, new_room("room_2", 15, 5));
-		push_back_room(&rooms, new_room("room_3", 5, 10));
-		push_back_room(&rooms, new_room("room_4", 10, 10));
-		push_back_room(&rooms, new_room("room_5", 20, 10));
-		push_back_room(&rooms, new_room("room_6", 0, 15));
-		push_back_room(&rooms, new_room("room_7", 5, 17));
-		find_room_by_name(rooms, "room_1")->status = START;
-		find_room_by_name(rooms, "room_0")->status = END;
-		add_union(rooms, "room_0", "room_6");
-		add_union(rooms, "room_0", "room_4");
-		add_union(rooms, "room_1", "room_2");
-		add_union(rooms, "room_1", "room_3");
-		add_union(rooms, "room_2", "room_7");
-		add_union(rooms, "room_2", "room_1");
-		add_union(rooms, "room_2", "room_5");
-		add_union(rooms, "room_3", "room_5");
-		add_union(rooms, "room_3", "room_1");
-		add_union(rooms, "room_4", "room_3");
-		add_union(rooms, "room_4", "room_2");
-		add_union(rooms, "room_5", "room_3");
-		add_union(rooms, "room_5", "room_2");
-		add_union(rooms, "room_5", "room_6");
-		add_union(rooms, "room_6", "room_5");
-		add_union(rooms, "room_6", "room_7");
-		add_union(rooms, "room_7", "room_6");
-		add_union(rooms, "room_7", "room_2");
-		add_union(rooms, "room_7", "room_4");
-		add_union(rooms, "room_7", "room_6");
-		add_union(rooms, "room_7", "room_2");
-		add_union(rooms, "room_7", "room_4");
-	}
-	if (test_number == 8)
-	{
-		push_back_room(&rooms, new_room("start", 10, 5));
-		push_back_room(&rooms, new_room("end", 10, 5));
-		push_back_room(&rooms, new_room("3", 15, 5));
-		push_back_room(&rooms, new_room("4", 5, 10));
-		push_back_room(&rooms, new_room("1", 10, 10));
-		push_back_room(&rooms, new_room("2", 15, 5));
-		push_back_room(&rooms, new_room("5", 5, 10));
-		push_back_room(&rooms, new_room("6", 10, 10));
-		find_room_by_name(rooms, "start")->status = START;
-		find_room_by_name(rooms, "end")->status = END;
-		add_union(rooms, "start", "1");
-		add_union(rooms, "3", "4");
-		add_union(rooms, "2", "4");
-		add_union(rooms, "1", "5");
-		add_union(rooms, "6", "5");
-		add_union(rooms, "end", "6");
-		add_union(rooms, "1", "2");
-		add_union(rooms, "2", "end");
-		add_union(rooms, "3", "start");
-	}
+
+
+	perform_test(8 , &rooms);
+
 	t_queue *queue;
 	t_room *start_room;
 
@@ -1018,4 +834,233 @@ int main()
 	free_pathes(&pathes);
 	free_rooms(&rooms);
 	return 0;
+}
+
+void	perform_test(int num, t_room **rooms)
+{
+	if (num == 1)
+	{
+		push_back_room(rooms, new_room("room_0", 10, 0));
+		push_back_room(rooms, new_room("room_1", 10, 0));
+		push_back_room(rooms, new_room("room_2", 10, 0));
+		push_back_room(rooms, new_room("room_3", 10, 0));
+		push_back_room(rooms, new_room("room_4", 10, 0));
+		push_back_room(rooms, new_room("room_5", 10, 0));
+		find_room_by_name(*rooms, "room_0")->status = START;
+		find_room_by_name(*rooms, "room_5")->status = END;
+		add_union(*rooms, "room_0", "room_1");
+		add_union(*rooms, "room_0", "room_2");
+		add_union(*rooms, "room_0", "room_3");
+		add_union(*rooms, "room_2", "room_1");
+		add_union(*rooms, "room_2", "room_3");
+		add_union(*rooms, "room_4", "room_1");
+		add_union(*rooms, "room_3", "room_4");
+		add_union(*rooms, "room_2", "room_4");
+		add_union(*rooms, "room_4", "room_5");
+	}
+	if (num == 2)
+	{
+		push_back_room(rooms, new_room("room_0", 10, 0));
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		push_back_room(rooms, new_room("room_7", 5, 17));
+		push_back_room(rooms, new_room("room_8", 0, 20));
+		push_back_room(rooms, new_room("room_9", 10, 25));
+		push_back_room(rooms, new_room("room_10", 15, 25));
+		find_room_by_name(*rooms, "room_0")->status = START;
+		find_room_by_name(*rooms, "room_9")->status = END;
+		add_union(*rooms, "room_0", "room_1");
+		add_union(*rooms, "room_0", "room_2");
+		add_union(*rooms, "room_2", "room_5");
+		add_union(*rooms, "room_2", "room_4");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_1", "room_4");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_4", "room_3");
+		add_union(*rooms, "room_3", "room_6");
+		add_union(*rooms, "room_3", "room_7");
+		add_union(*rooms, "room_4", "room_9");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_6", "room_8");
+		add_union(*rooms, "room_8", "room_9");
+		add_union(*rooms, "room_7", "room_9");
+		add_union(*rooms, "room_9", "room_10");
+	}
+	if (num == 3)
+	{
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		push_back_room(rooms, new_room("room_7", 5, 17));
+		find_room_by_name(*rooms, "room_1")->status = START;
+		find_room_by_name(*rooms, "room_7")->status = END;
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_1", "room_6");
+		add_union(*rooms, "room_1", "room_4");
+		add_union(*rooms, "room_2", "room_3");
+		add_union(*rooms, "room_2", "room_4");
+		add_union(*rooms, "room_4", "room_6");
+		add_union(*rooms, "room_2", "room_5");
+		add_union(*rooms, "room_6", "room_5");
+		add_union(*rooms, "room_6", "room_7");
+	}
+	if (num == 4)
+	{
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		find_room_by_name(*rooms, "room_1")->status = START;
+		find_room_by_name(*rooms, "room_6")->status = END;
+		add_union(*rooms, "room_1", "room_2");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_2", "room_3");
+		add_union(*rooms, "room_2", "room_4");
+		add_union(*rooms, "room_4", "room_5");
+		add_union(*rooms, "room_5", "room_6");
+	}
+	if (num == 5)
+	{
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		push_back_room(rooms, new_room("room_7", 5, 17));
+		push_back_room(rooms, new_room("room_8", 0, 20));
+		push_back_room(rooms, new_room("room_9", 10, 25));
+		push_back_room(rooms, new_room("room_10", 15, 25));
+		push_back_room(rooms, new_room("room_11", 15, 25));
+		push_back_room(rooms, new_room("room_12", 15, 25));
+		push_back_room(rooms, new_room("room_13", 15, 25));
+		find_room_by_name(*rooms, "room_1")->status = START;
+		find_room_by_name(*rooms, "room_13")->status = END;
+		add_union(*rooms, "room_1", "room_2");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_2", "room_3");
+		add_union(*rooms, "room_2", "room_4");
+		add_union(*rooms, "room_3", "room_6");
+		add_union(*rooms, "room_4", "room_6");
+		add_union(*rooms, "room_1", "room_5");
+		add_union(*rooms, "room_4", "room_7");
+		add_union(*rooms, "room_6", "room_8");
+		add_union(*rooms, "room_7", "room_5");
+		add_union(*rooms, "room_5", "room_8");
+		add_union(*rooms, "room_5", "room_9");
+		add_union(*rooms, "room_9", "room_10");
+		add_union(*rooms, "room_9", "room_11");
+		add_union(*rooms, "room_10", "room_12");
+		add_union(*rooms, "room_11", "room_12");
+		add_union(*rooms, "room_12", "room_13");
+	}
+	if (num == 6)
+	{
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		push_back_room(rooms, new_room("room_7", 0, 15));
+		push_back_room(rooms, new_room("room_8", 0, 15));
+		push_back_room(rooms, new_room("room_9", 0, 15));
+		find_room_by_name(*rooms, "room_1")->status = START;
+		find_room_by_name(*rooms, "room_6")->status = END;
+		add_union(*rooms, "room_1", "room_2");
+		add_union(*rooms, "room_1", "room_4");
+		add_union(*rooms, "room_2", "room_3");
+		add_union(*rooms, "room_3", "room_6");
+		add_union(*rooms, "room_4", "room_5");
+		add_union(*rooms, "room_5", "room_7");
+		add_union(*rooms, "room_7", "room_8");
+		add_union(*rooms, "room_8", "room_9");
+		add_union(*rooms, "room_9", "room_6");
+	}
+	if (num == 7)
+	{
+		push_back_room(rooms, new_room("room_0", 10, 5));
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		push_back_room(rooms, new_room("room_7", 5, 17));
+		find_room_by_name(*rooms, "room_1")->status = START;
+		find_room_by_name(*rooms, "room_0")->status = END;
+		add_union(*rooms, "room_0", "room_6");
+		add_union(*rooms, "room_0", "room_4");
+		add_union(*rooms, "room_1", "room_2");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_2", "room_7");
+		add_union(*rooms, "room_2", "room_1");
+		add_union(*rooms, "room_2", "room_5");
+		add_union(*rooms, "room_3", "room_5");
+		add_union(*rooms, "room_3", "room_1");
+		add_union(*rooms, "room_4", "room_3");
+		add_union(*rooms, "room_4", "room_2");
+		add_union(*rooms, "room_5", "room_3");
+		add_union(*rooms, "room_5", "room_2");
+		add_union(*rooms, "room_5", "room_6");
+		add_union(*rooms, "room_6", "room_5");
+		add_union(*rooms, "room_6", "room_7");
+		add_union(*rooms, "room_7", "room_6");
+		add_union(*rooms, "room_7", "room_2");
+		add_union(*rooms, "room_7", "room_4");
+		add_union(*rooms, "room_7", "room_6");
+		add_union(*rooms, "room_7", "room_2");
+		add_union(*rooms, "room_7", "room_4");
+	}
+	if (num == 8)
+	{
+		push_back_room(rooms, new_room("start", 10, 5));
+		push_back_room(rooms, new_room("end", 10, 5));
+		push_back_room(rooms, new_room("3", 15, 5));
+		push_back_room(rooms, new_room("4", 5, 10));
+		push_back_room(rooms, new_room("1", 10, 10));
+		push_back_room(rooms, new_room("2", 15, 5));
+		push_back_room(rooms, new_room("5", 5, 10));
+		push_back_room(rooms, new_room("6", 10, 10));
+		find_room_by_name(*rooms, "start")->status = START;
+		find_room_by_name(*rooms, "end")->status = END;
+		add_union(*rooms, "start", "1");
+		add_union(*rooms, "3", "4");
+		add_union(*rooms, "2", "4");
+		add_union(*rooms, "1", "5");
+		add_union(*rooms, "6", "5");
+		add_union(*rooms, "end", "6");
+		add_union(*rooms, "1", "2");
+		add_union(*rooms, "2", "end");
+		add_union(*rooms, "3", "start");
+	}
+	if (num == 9)
+	{
+		push_back_room(rooms, new_room("room_0", 10, 5));
+		push_back_room(rooms, new_room("room_1", 10, 5));
+		push_back_room(rooms, new_room("room_2", 15, 5));
+		push_back_room(rooms, new_room("room_3", 5, 10));
+		push_back_room(rooms, new_room("room_4", 10, 10));
+		push_back_room(rooms, new_room("room_5", 20, 10));
+		push_back_room(rooms, new_room("room_6", 0, 15));
+		find_room_by_name(*rooms, "room_0")->status = START;
+		find_room_by_name(*rooms, "room_6")->status = END;
+		add_union(*rooms, "room_0", "room_1");
+		add_union(*rooms, "room_0", "room_2");
+		add_union(*rooms, "room_1", "room_3");
+		add_union(*rooms, "room_2", "room_3");
+		add_union(*rooms, "room_3", "room_4");
+		add_union(*rooms, "room_3", "room_5");
+		add_union(*rooms, "room_4", "room_6");
+		add_union(*rooms, "room_5", "room_6");
+	}
 }
