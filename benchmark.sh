9e0122ae2df4ac8b0254476aafaf63e3da5e6801
hcaterpi <hcaterpi@student.21-school.ru>
2020-08-06 14:10:21 +0300

    Added convenient benchmark and the example of kinda problem map

---
+++ benchmark.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+    then
+    echo "Usage: ./benchmark.sh [mode] [repeats]"
+    echo "	mode - the generator mode from 1 (--flow-one) to 5 (--big-superposition)"
+    echo "	repeats - the number of tests"
+    exit 1
+elif [ "$1" -eq 1 ]
+    then
+    MODE="--flow-one"
+elif [ "$1" -eq 2 ]
+    then
+    MODE="--flow-ten"
+elif [ "$1" -eq 3 ]
+    then
+    MODE="--flow-thousand"
+elif [ "$1" -eq 4 ]
+    then
+    MODE="--big"
+elif [ "$1" -eq 5 ]
+    then
+    MODE="--big-superposition"
+fi
+
+if [ $# -eq 1 ]
+    then
+    REPEATS=5
+    else
+    REPEATS="$2"
+fi
+
+
+for (( i=1; i <= $REPEATS; i++ ))
+do
+		NUMBER_MAP="$(maps/generator $MODE > bench.map && cat bench.map | sed -n '2 p' | tr -dc '0-9')"
+        NUMBER_LEMIN="$(./lem-in < bench.map | grep -c '^L')"
+		if [ "$NUMBER_MAP" -lt "$NUMBER_LEMIN" ]
+			then
+            echo "$i KO: $(($NUMBER_LEMIN - NUMBER_MAP))"
+			cat test.map >> problems
+		    else
+            echo "$i OK"
+		fi
+done
+
+rm bench.map
